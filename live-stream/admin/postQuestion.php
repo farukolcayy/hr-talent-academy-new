f0725fb6889de48cd8fb461ab53d3b63d83c1519
ömer faruk Olcay <53448620+farukolcayy@users.noreply.github.com>
2023-03-17 17:17:54 +0300

    public update

--- live-stream/admin/postQuestion.php
+++ live-stream/admin/postQuestion.php
@@ -4,7 +4,7 @@ $question = $_POST['_question'];
 $questionOptions = $_POST['_questionOptions'];
 $questionAnswer = $_POST['_questionAnswer'];
 
-$conn = new PDO('mysql:host=localhost;dbname=hrtalent_live;charset=utf8;port=3306', 'hrtalent_basvuru', '!hrt!2021!');
+$conn = new PDO(-);
 
 $query = $conn->prepare("INSERT INTO oturum_sinav_soru SET question= ?,questionOption= ?,answer= ?");
 
